5720fd81bdf75c9c6d6fe8b3bc6cb90993cd8e0a
Velaga Naga Bhavana <velaganagabhavana09@gmail.com>
2024-12-10 21:55:18 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+
+export const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '0cd61c6ae9mshc45cb2f7b436100p197248jsn3677810024e4',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="b630ad4741cc9d267bc8da9eaa94e31c";
+
